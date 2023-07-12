dd65b9d78843a8684449e094f8e11d7c6b8fc992
Mahmoud El Bergui <46870458+MaHmOuDeB@users.noreply.github.com>
2023-07-12 13:25:58 +0200

    first commit

---
+++ cc-project/main.py
@@ -0,0 +1,50 @@
+from fastapi import FastAPI, Response, HTTPException
+import pandas as pd
+
+from utils import RESULT_PATH
+
+app = FastAPI()
+
+@app.get('/')
+async def home():
+    message = {'message': 'Welcome to NYSE historic data API. To request data of stocks go to /data/[ticker]'}
+    return Response(content=str(message), media_type='application/json')
+
+@app.get('/credits')
+async def credits():
+    message = {'credits': 'This REST API is a project done by Alejandro Rodriguez & Mahmoud El Bergui'}
+    return Response(content=str(message), media_type='application/json')
+
+@app.get('/data/{ticker}')
+async def retrieve_data(ticker:str):
+    """
+    Search stock history of company using ACT Symbol (ticker) or Company's name as reference.
+    """
+    ticker = ticker.upper()
+    df = pd.read_csv(RESULT_PATH).query('ticker == @ticker or company == @ticker')
+    
+    search_term = 'Apple'
+    
+    if search_term in df['Company'].values:
+        print(f"{search_term} is found in the file.")
+    else:
+        print(f"{search_term} is not found in the file.")
+    
+    if not df.empty:
+        # Split DataFrame into column and data lists
+        cols = df.columns.values.tolist()
+        data = df.values.tolist()
+
+        # Insert column list at the beginning of the data list
+        data.insert(0, cols)
+
+        # Combine list of list into a single text
+        cont = '\n'.join([','.join(map(str, row)) for row in data])
+
+        return Response(content=cont, media_type='text/csv')
+    
+    message = f'{ticker} could not be found in the NYSE registry. Please make sure the name is correct.'
+    return HTTPException(status_code=404, detail=message)
+
+##Find file
+##rest api is not working in the credits section.
